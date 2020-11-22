c12a3d398191da8fc5e228ac83c0ce91eaf1af3c
youliangdao <youliangdaojing5@gmail.com>
2020-11-22 15:26:20 +0900

    テストファイル作成時の自動生成ファイルの設定

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,11 @@ module Connectter
     config. generators do |g|
       g.assets false
       g.skip_routes false
-      g.test_framework false
+      g.test_framework :rspec,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
