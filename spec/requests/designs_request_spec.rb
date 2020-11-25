20df0e98a69226c73042560a41f840e9e3df0a3f
youliangdao <youliangdaojing5@gmail.com>
2020-11-25 12:36:15 +0900

    静的ページのリクエストテストの実行

--- spec/requests/designs_request_spec.rb
+++ spec/requests/designs_request_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "Designs", type: :request do
+RSpec.describe "静的ページ", type: :request do
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/designs/index"
-      expect(response).to have_http_status(:success)
+  describe "トップページ" do
+    it "正常なレスポンスを返すこと" do
+      get root_path
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
     end
   end
 
