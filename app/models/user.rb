c852ff49726cda4e3946696563ef7358b39093b6
youliangdao <youliangdaojing5@gmail.com>
2020-12-22 16:29:58 +0900

    ユーザーテーブルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: ture do
+
+  end
 end
